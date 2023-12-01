a9801d1fa4eb65cece9aa910ae00462dc2d6ee81
The-Coder-AJ <anshjain722@gmail.com>
2023-12-01 17:48:12 +0530

    modified:   react-app/index.html 	modified:   react-app/src/App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function Square({ value }){
+  return <button className="square">{value}</button>;
+}
+export default function Board() {
+  const name = 'ansh';
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="board-ow">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
     </>
-  )
+  );
 }
-
-export default App
