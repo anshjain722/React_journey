61680da47a16190c7b6a3e7a88ef9bf34a25fccd
Ansh Jain <anshjain722@gmail.com>
2023-12-01 21:51:10 +0530

    added Lists

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,14 @@
+function ListGroup() {
+  return (
+    <>
+        <h1>This is a list created in react. Fuck you😊</h1>
+        <ul className="list-group">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">second item</li>
+          <li className="list-group-item">third item</li>
+          <li className="list-group-item">fourth item</li>
+        </ul>
+    </>
+  );
+}
+export default ListGroup;
